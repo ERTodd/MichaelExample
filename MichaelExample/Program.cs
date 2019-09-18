48eefa924250a555308ecab879883d8bcd3d2f90
Elena Todd <elenatwhite@gmail.com>
2019-09-18 10:55:01 -0500

    Add project files.

---
+++ MichaelExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MichaelExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
